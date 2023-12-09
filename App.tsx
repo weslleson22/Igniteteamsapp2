7d18e903f420eeb7bdb36479f1fae62c596061a9
weslleson22 <wesllesonsowza@gamil.com>
2023-12-09 08:20:47 -0300

    nova branch

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Groups } from './src/screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import theme from './src/theme';
 
 export default function App() {
   return (
-   <Groups></Groups>
+    <ThemeProvider theme={theme}>
+        <Groups></Groups>
+   </ThemeProvider>
+   
+    
+
   );
 }
 
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,7 @@ import { Container, Title } from "./styles";
 export function Groups(){
     return (
         <Container>
-            <Title>
-                Groups!
-            </Title>    
+
         </Container>
     );
 }
